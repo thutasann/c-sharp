1436fd49e60180e9ef62aa2152bb38bccc7b1632
thutasann <thutasann2002@gmail.com>
2023-12-12 22:52:29 +0800

    feat: variables

--- cs-crash/Program.cs
+++ cs-crash/Program.cs
@@ -6,7 +6,29 @@ namespace MyFirstProgram
     {
         static void Main(string[] args)
         {
-            Console.Beep();
+            int x; // declaratin
+            x = 123; // initialization
+
+            int y = 123; // declaration + initialization
+
+            int z = x + y;
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+
+            int age = 21;
+            double height = 300.4;
+            bool male = true;
+            Console.WriteLine("Your age is " + age);
+            Console.WriteLine("Your Height is " + height);
+            Console.WriteLine("Your Gender is " + male);
+
+            char sign = '@';
+            Console.WriteLine("This is sign " + sign);
+
+            string name = "Bro code";
+            Console.WriteLine("This is String" +  name);
         }
     }
 }

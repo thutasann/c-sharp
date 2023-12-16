bd4b389a4bb514af0584773ef050c4b1f52d2498
thutasann <thutasann2002@gmail.com>
2023-12-16 16:30:33 +0800

    feat: number gussing game

--- cs-crash/Program.cs
+++ cs-crash/Program.cs
@@ -193,6 +193,56 @@ namespace MyFirstProgram
                 Console.WriteLine();
             }
 
+            // Number gussing game
+            Random randomGame = new Random();
+            bool playAgain = true;
+            int min = 1;
+            int max = 100;
+            int guess;
+            int number;
+            int guesses;
+            string response;
+
+            while (playAgain)
+            {
+                guess = 0;
+                guesses = 100;
+                response = "";
+                number = randomGame.Next(min, max + 1);
+
+                while(guess != number)
+                {
+                    Console.WriteLine("Guess a number between " + min + " - " + max);
+                    guess = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Guess: " + guess);
+
+                    if(guess > number) {
+                        Console.WriteLine(guess + " is to high!");
+                    }
+                    else if(guess < number)
+                    {
+                        Console.WriteLine(guess + " is to low!");
+                    }
+                    guess++;
+                }
+                Console.WriteLine("Number: " + number);
+                Console.WriteLine("YOU WIN!");
+                Console.WriteLine("Gusses: " + guesses);
+                Console.WriteLine("Would you like to play again (Y/N): ");
+                response = Console.ReadLine();
+                response = response.ToUpper();
+
+                if(response == "Y")
+                {
+                    playAgain = true;
+                }
+                else
+                {
+                    playAgain = false;
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("Thanks for playing!... I Guess");
 
             Console.ReadKey();
         }

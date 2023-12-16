ab1ee481b9afbddf1e4268a33313044265b30ed6
thutasann <thutasann2002@gmail.com>
2023-12-16 20:17:01 +0800

    feat: array for forloop

--- cs-crash/Program.cs
+++ cs-crash/Program.cs
@@ -6,57 +6,29 @@ namespace MyFirstProgram
     {
         static void Main(string[] args)
         {
-            do
+            string[] cars = { "BMW", "Mustang", "Corvette" };
+            cars[0] = "BMW Updated";
+
+            for(int i = 0; i < cars.Length; i++)
             {
-                double num1 = 0;
-                double num2 = 0;
-                double result = 0;
+                Console.WriteLine(cars[i]);
+            }
 
-                Console.WriteLine("------------");
-                Console.WriteLine("Calculator program");
-                Console.WriteLine("------------");
+            string[] cycles = new string[3];
 
-                Console.WriteLine("Enter Number 1: ");
-                num1 = Convert.ToDouble(Console.ReadLine());
+            cycles[0] = "Bike 1";
+            cycles[1] = "Bike 2";
+            cycles[2] = "Bike 3";
 
-                Console.WriteLine("Enter Number 2 : ");
-                num2 = Convert.ToDouble(Console.ReadLine());
+            for(int i = 0; i < cycles.Length; i++)
+            {
+                Console.WriteLine(cycles[i]);
+            }
 
-                Console.WriteLine("Enter an option: ");
-                Console.WriteLine("\t+: " + "Add");
-                Console.WriteLine("\t-: " + "Subtract");
-                Console.WriteLine("\t*: " + "Multiply");
-                Console.WriteLine("\t/: " + "Divide");
-
-                switch (Console.ReadLine())
-                {
-                    case "+":
-                        result = num1 + num2;
-                        Console.WriteLine($"Your result : {num1} + {num2} = " + result);
-                        break;
-                    case "-":
-                        result = num1 - num2;
-                        Console.WriteLine($"Your result : {num1} - {num2} = " + result);
-                        break;
-                    case "*":
-                        result = num1 * num2;
-                        Console.WriteLine($"Your result : {num1} * {num2} = " + result);
-                        break;
-                    case "/":
-                        result = num1 / num2;
-                        Console.WriteLine($"Your result : {num1} / {num2} = " + result);
-                        break;
-                    default:
-                        Console.WriteLine("That was not a valid option");
-                        break;
-                }
-                Console.WriteLine("Would you like to continue? (Y = yes, N = NO): ");
-            } while (Console.ReadLine().ToUpper() == "Y");
-
-            Console.WriteLine("Bye");
-            Console.ReadKey();
-
-
+            foreach(string cycle in cycles)
+            {
+                Console.WriteLine(cycle);
+            }
         }
     }
 }
